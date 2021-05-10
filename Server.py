024c7ae820e811af595a87436a6341fb216e0f7e
Gregory Maldonado <gmaldona@oswego.edu>
2021-05-10 13:58:05 -0400

    gets bytes from vehicle and turns bytearray into hex values

--- Server.py
+++ Server.py
@@ -10,7 +10,7 @@ class Server:
 
     devices = []
     
-    def __init__(self, host='127.0.0.1', port=6000):
+    def __init__(self, host='192.168.1.100', port=6000):
         self._HOST = host
         self._PORT = port
         self.clientVehicles = []
@@ -64,14 +64,20 @@ class Server:
             devices = await BleakScanner.discover()
             for device in devices:
                 if 'Drive' in device.__str__():
-                    print(device.metadata['manufacturer_data'])
-                    manufacturer_data = hex(device.metadata['manufacturer_data'])
-                    print(manufacturer_data)
+                    starting_bytes = b'\xbe\xef'
+                    manufacturer_data = list(device.metadata['manufacturer_data'].values())[0]
+                    starting_hex = starting_bytes.hex()
+                    manufacturer_hex = manufacturer_data.hex()
+                    hex_value = starting_hex + manufacturer_hex
+                    hex_value = hex(int(hex_value, 16))
+                    byte_array = bytearray.fromhex(hex_value[2:])
+                    hex_value = bytearray.hex(byte_array)
+                    print(hex_value)
                     address = ''
                     for addr in device.address.split('-'):
                         address = address + addr
-                    self.devices.append(address.lower())
-
+                    self.devices.append({'address': address, 'manufacturer_data': hex_value})
+                    
         loop = asyncio.get_event_loop()
         loop.run_until_complete(discover())
         self.connection.send("SCAN;COMPLETED\n".encode())
